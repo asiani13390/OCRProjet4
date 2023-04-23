70081a443b4e509e39f0ade760c97b49cad6b9ca
Alain SIANI <asiani@free.fr>
2023-04-23 02:31:07 +0200

    Mise à jour du fichier : Fenetre modale

--- index.js
+++ index.js
@@ -34,7 +34,17 @@ function initialize()
     Url = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=" + categories[2];
     document.getElementById("category3").textContent = categories[2];
     addPhotosToContainer(Container_id, Url);
-    
+
+    // Manage modal window
+
+    let modal = document.getElementById("myModal");
+    let span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on X, close the modal
+    span.onclick = function() 
+    {
+        modal.style.display = "none";
+    }
 }
 
 
@@ -131,4 +141,53 @@ function left_shift(container_id)
     container.style.transform="translate(" + photo_width*position + "px)";
 
     position_index_for_carousel[container_id] = position;
+}
+
+function open_modal(article_id) {
+    get_data_of_modal(article_id);
+    let modal = document.getElementsByClassName("modal")[0];
+    modal.style.display = "block";
+}
+
+async function get_data_of_modal(article_id)
+{
+  url_api = "http://localhost:8000/api/v1/titles/" + article_id;
+  let data_from_api =  await get_articles(url_api);
+
+  let modal_image_url = document.getElementById("modal_image_url");
+  modal_image_url.onerror=function() { modal_image_url.src = "indisponible.jpg"; }
+  modal_image_url.setAttribute("src", data_from_api.image_url);
+
+  let modal_title = document.getElementById("modal_title");
+  modal_title.textContent = data_from_api.title;
+
+  let modal_genres = document.getElementById("modal_genres");
+  modal_genres.textContent = data_from_api.genres;
+
+  let modal_published_date = document.getElementById("modal_published_date");
+  modal_published_date.textContent = data_from_api.date_published;
+
+  let modal_rated = document.getElementById("modal_rated");
+  modal_rated.textContent = data_from_api.rated;
+  
+  let modal_imdb_score = document.getElementById("modal_imdb_score");
+  modal_imdb_score.textContent = data_from_api.imdb_score;
+
+  let modal_directors = document.getElementById("modal_directors");
+  modal_directors.textContent = data_from_api.directors;
+  
+  let modal_actors = document.getElementById("modal_actors");
+  modal_actors.textContent = data_from_api.actors;
+
+  let modal_duration = document.getElementById("modal_duration");
+  modal_duration.textContent = data_from_api.duration;
+
+  let modal_countries = document.getElementById("modal_countries");
+  modal_countries.textContent = data_from_api.countries;
+
+  let modal_votes = document.getElementById("modal_votes");
+  modal_votes.textContent = data_from_api.votes;
+
+  let modal_long_description = document.getElementById("modal_long_description");
+  modal_long_description.textContent = data_from_api.long_description;
 }
